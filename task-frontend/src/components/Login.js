0fd8391092a9009e71b603c5b3378e3ca3c6c559
Student <venus.kabera@student.moringaschool.com>
2023-03-05 19:29:30 +0300

    login linked

--- task-frontend/src/components/Login.js
+++ task-frontend/src/components/Login.js
@@ -1,23 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-function Login (){
-    return(
-        <div>
-            <form>
-                <input type="text" placeholder="Enter Name" /><br />
-                <input type="email" placeholder="Enter Email"/><br />
-                <input  type="password" placeholder="Enter Password"/><br />
-                <Link to="/Task"><button type="submit">Log In</button></Link>
-                
-                <span>
-                    <h6>If you don't have an account</h6>
-                    <Link to="signup"><h6>Register Now!</h6></Link>
 
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-                </span>
-            </form>
-        </div>
-    )
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    fetch("http://127.0.0.1:9292/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {                                                                                                 
+        if (data.code === 200) {
+
+          console.log("Login successful!");
+        } else {
+          console.log("Login failed: " + data.data.message);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Email:
+          <input type="email" name="email" value={email} onChange={handleEmailChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" name="password" value={password} onChange={handlePasswordChange} />
+        </label>
+        <br />
+        <Link to="/Task"><button type="submit">Log In</button></Link>
+      </form>
+    </div>
+  );
 }
-export default Login
+
+export default Login;
