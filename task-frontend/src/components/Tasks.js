fbe9cf882c0fe289fe89dee913ffd3bc65d7f81e
Student <venus.kabera@student.moringaschool.com>
2023-03-06 13:26:43 +0300

    safe

--- task-frontend/src/components/Tasks.js
+++ task-frontend/src/components/Tasks.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from "react";
+import  '../App.css';
 
-function Tasks() {
+function TaskList() {
   const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState({ name: "", description: "", due: "" });
+  const [editingTaskId, setEditingTaskId] = useState(null);
+  const [newTask, setNewTask] = useState({
+    name: "",
+    description: "",
+    due: "",
+  });
 
   useEffect(() => {
     fetch("https://venus-backend-hhw4.onrender.com/tasks")
@@ -21,22 +27,63 @@ function Tasks() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    fetch("https://venus-backend-hhw4.onrender.com/tasks/create", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newTask),
-    })
-      .then((response) => response.json())
-      .then((data) => setTasks([...tasks, data.data]))
-      .catch((error) => console.log(error));
+    if (editingTaskId) {
+      // Update existing task
+      fetch(
+        `https://venus-backend-hhw4.onrender.com/tasks/${editingTaskId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newTask),
+        }
+      )
+        .then((response) => response.json())
+        .then((data) =>
+          setTasks((prevState) =>
+            prevState.map((task) => (task.id === data.data.id ? data.data : task))
+          )
+        )
+        .catch((error) => console.log(error));
+      setEditingTaskId(null);
+    } else {
+      // Add new task
+      fetch("https://venus-backend-hhw4.onrender.com/tasks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newTask),
+      })
+        .then((response) => response.json())
+        .then((data) => setTasks([...tasks, data.data]))
+        .catch((error) => console.log(error));
+    }
 
-    setNewTask({ name: "", description: "" , due: ""});
+    setNewTask({ name: "", description: "", due: "" });
+  };
+
+  const handleEditClick = (task) => {
+    setEditingTaskId(task.id);
+    setNewTask({
+      name: task.name,
+      description: task.description,
+      due: task.due ? new Date(task.due).toISOString().substr(0, 10) : "",
+    });
+  };
+
+  const handleDeleteClick = (taskId) => {
+    fetch(`https://venus-backend-hhw4.onrender.com/tasks/${taskId}`, {
+      method: "DELETE",
+    })
+      .then(() => setTasks((prevState) => prevState.filter((task) => task.id !== taskId)))
+      .catch((error) => console.log(error));
   };
 
   return (
-    <div>
+    <div className="task-list">
+      <h1>Task List</h1>
       <form onSubmit={handleSubmit}>
         <label>
           Task Name:
@@ -46,7 +93,7 @@ function Tasks() {
             value={newTask.name}
             onChange={handleInputChange}
           />
-        </label> <br/>
+        </label>
         <label>
           Task Description:
           <textarea
@@ -54,28 +101,43 @@ function Tasks() {
             value={newTask.description}
             onChange={handleInputChange}
           />
-        </label> <br/>
-        
+        </label>
         <label>
           Due Date:
-          <textarea
+          <input
+            type="date"
             name="due"
             value={newTask.due}
             onChange={handleInputChange}
           />
-          <button type="submit"> Add Task</button> <br/>
-      
         </label>
-      </form>
-      {tasks.map((task) => (
-        <div key={task.id}>
-          <h2>Task Name:{task.name}</h2>
-          <p>Task Description {task.description}</p>
-          <p>Due: {task.due}</p>
-        </div>
-      ))}
-    </div>
-  );
+        <button type="submit">{editingTaskId ? "Update Task" : "Add Task"}</button>
+</form>
+<table>
+<thead>
+<tr>
+<th>Task Name</th>
+<th>Task Description</th>
+<th>Due Date</th>
+<th>Actions</th>
+</tr>
+</thead>
+<tbody>
+{tasks.map((task) => (
+<tr key={task.id}>
+<td>{task.name}</td>
+<td>{task.description}</td>
+<td>{task.due ? new Date(task.due).toLocaleDateString() : "-"}</td>
+<td>
+<button onClick={() => handleEditClick(task)}>Edit</button>
+<button onClick={() => handleDeleteClick(task.id)}>Delete</button>
+</td>
+</tr>
+))}
+</tbody>
+</table>
+</div>
+);
 }
 
-export default Tasks;
+export default TaskList;
